e9320e1d5cfef411f065ec7a7371f381133f900b
Sergio <sergioteovit@gmail.com>
2025-10-29 18:30:21 -0600

    Se actualiza menu

--- E1/index.php
+++ E1/index.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Órganos y Sistemas</title>
     <!-- Incluye el CSS de Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <!--link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/mainmenu.css">
 </head>
 <body>
 
+    <p>Para comenzar selecciona una región</p>
+    
 <div class="container">
     <div class="row">
         <?php
@@ -32,16 +34,23 @@
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
+            echo '<div class="carousel">';
             while ($row = $result->fetch_assoc()) {
-                echo '<div class="card col-12 col-sm-6 col-md-3 mb-3">';
+                /* echo '<div class="card col-12 col-sm-6 col-md-3 mb-3">';
                 echo '<img src="icons/main/' . $row["Icon"] . '" class="card-img-top bg-info icon-btn" alt="...">';
                 echo '<div class="card-body">';
                 echo '<h5 class="card-title">' . $row["System"] . "</h5>";
                 echo '<p class="card-text">Descripción general.</p>';
                 echo '<a href="region/' . $row["Url"] . '.php" class="btn btn-primary">Iniciar</a>';
                 echo "</div>";
-                echo "</div>";
+                echo "</div>";*/
+                
+                echo '<div class="carousel-item">';
+                echo '<img src="icons/main/'.$row["Icon"].'" alt="Dog" title="Dog"/>';
+                echo '<h3>'.$row["System"].'<br/><a class="waves-effect waves-light btn btn-large light-blue accent-2" href="region/' . $row["Url"] . '.php" width="100%">Seleccionar</a></h3>';
+                echo '</div>';
             }
+            echo '</div>';
         } else {
             echo "0 resultados";
         }
@@ -53,27 +62,11 @@
 </div>
 
 <!-- Incluye el JavaScript de Bootstrap para algunas funcionalidades (opcional en este caso) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!--script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></!--script-->
     
-    <div class="containercarrousel">
-      <div class="carousel">
-        <div class="item a">A</div>
-        <div class="item b">B</div>
-        <div class="item c">C</div>
-        <div class="item d">D</div>
-        <div class="item e">E</div>
-        <div class="item f">F</div>
-          <div class="item g">G</div>
-          <div class="item h">H</div>
-          <div class="item i">I</div>
-          <div class="item j">J</div>
-          <div class="item k">K</div>
-          <div class="item l">L</div>
-      </div>
-    </div>
-    <div class="next">Next</div>
-    <div class="prev">Prev</div>
-    
-<script src="./js/mainmenu.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="./js/mainmenu.js"></script>
+
 </body>
 </html>
