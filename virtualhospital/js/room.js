5bfd16013806f2328e2bd027b41ee80553fefcd9
Sergio Teodoro Vite <sergioteovit@gmail.com>
2025-11-16 01:57:08 -0600

    Se actualizan interacciones
    Ahora es posible abrir y cerrar puertas, así como apagar y encender luces

--- virtualhospital/js/room.js
+++ virtualhospital/js/room.js
@@ -7,15 +7,41 @@ let camera;
 let renderer;
 let controls;
 
+let ambientlight, Hlight;
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+const doorsList = [
+    "PTABANIO",
+    "PTABANIO60cm",
+    "PTACIRUGIA1",
+    "PTACONSULTORIO",
+    "PTACONSULTORIOVENTANA",
+    "PTADOBLE",
+    "PTADOBLEQX",
+    "PTAOFICINA",
+    "PTAQX",
+    "PTARADIOLOGIA",
+    "PTASANITARIO",
+    "PTASANITARIO2",
+    "PTASANITARIO3",
+    "PTAVENTANACIRC"
+];
+
+const lampsList = ["TLAMP", "LAMP"];
+let lightsMap = new Map();
+let lightsOn = 0;
+let numLights = 0;
+
 function readSceneData() {
     fetch("xml/" + xmlfile + ".xml")
         .then((response) => response.text()) // Get the response as text
         .then((xmlString) => {
             // Process the XML string here
-            console.log("XML Loaded ok...");
+            // console.log("XML Loaded ok...");
             const parser = new DOMParser();
             const xmlDoc = parser.parseFromString(xmlString, "text/xml");
-            console.log("Root element name:", xmlDoc.documentElement.nodeName);
+            // console.log("Root element name:", xmlDoc.documentElement.nodeName);
 
             let rootData = {
                 name: xmlDoc.documentElement.getAttribute("name"),
@@ -76,23 +102,41 @@ function loadFBX(objectData) {
 
             object.position.set(objectData.x, objectData.y, objectData.z);
 
-            if (objectData.name == "TLAMP") {
+            object.name = objectData.name;
 
-                const spotLight = new THREE.SpotLight(0xffffff, 2, 10, Math.PI/2.1, 0.1, 2);
-                
-                spotLight.position.set(objectData.x, objectData.y+2.8, objectData.z);
+            if (lampsList.includes(objectData.name)) {
+                const spotLight = new THREE.SpotLight(0xffffff, 1, 20, Math.PI / 2.5, 0.5, 1.5);
+
+                spotLight.position.set(objectData.x, objectData.y + 2.8, objectData.z);
                 const targetl = new THREE.Object3D();
                 targetl.position.set(objectData.x, 0, objectData.z);
                 spotLight.target = targetl;
+
                 scene.add(targetl);
                 scene.add(spotLight);
+
+                object.userData.isTurnedOn = true;
+
+                lightsMap.set(object.uuid, spotLight.uuid);
+                // console.log(object.uuid + " => " + spotLight.uuid);
+
+                numLights++;
+                lightsOn++;
+
                 //const spotLightHelper = new THREE.SpotLightHelper(spotLight);
                 //scene.add(spotLightHelper);
-
+            } else if (doorsList.includes(objectData.name)) {
+                object.userData.isOpened = false;
             }
+            // console.log(object);
 
             scene.add(object);
-            console.log("models/" + objectData.name + ".fbx  loaded");
+
+            // console.log("models/" + objectData.name + ".fbx  loaded");
+
+            /*for (const [key, value] of lightsMap.entries()) {
+                console.log(`Key: ${key}, Value: ${value}`);
+            }*/
         },
         function (progress) {
             console.log((progress.loaded / progress.total) * 100 + "% cargado");
@@ -104,6 +148,59 @@ function loadFBX(objectData) {
             alert("Error al cargar el modelo FBX. Por favor, intenta con otro archivo.");
         }
     );
+}
+
+function onDoubleClick(event) {
+    event.preventDefault();
+
+    // Calculate mouse position in normalized device coordinates (-1 to +1)
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObjects(scene.children, true); // `true` for recursive check
+
+    if (intersects.length > 0) {
+        const object = intersects[0].object.parent;
+
+        // console.log("Clicked object:", object);
+        if (object.isGroup) {
+            if (doorsList.includes(object.name)) {
+                // console.log("The object is a Door.");
+                if (object.userData.isOpened) {
+                    object.rotateY(-Math.PI / 2);
+                    object.userData.isOpened = false;
+                } else {
+                    object.rotateY(Math.PI / 2);
+                    object.userData.isOpened = true;
+                }
+            } else if (lampsList.includes(object.name)) {
+                //console.log("The object is a light.");
+                //console.log("ID: " + object.uuid + " light: " + lightsMap.get(object.uuid));
+                if (object.userData.isTurnedOn) {
+                    const light = scene.getObjectByProperty("uuid", lightsMap.get(object.uuid));
+                    light.intensity = 0;
+                    object.userData.isTurnedOn = false;
+                    lightsOn--;
+                    //console.log(light);
+                    ambientlight.intensity = (0.1 * lightsOn) / numLights;
+                    Hlight.intensity = (0.2 * lightsOn) / numLights;
+                } else {
+                    const light = scene.getObjectByProperty("uuid", lightsMap.get(object.uuid));
+                    light.intensity = 1;
+                    object.userData.isTurnedOn = true;
+                    lightsOn++;
+                    //console.log(light);
+                    ambientlight.intensity = (1.0 * lightsOn) / numLights;
+                    Hlight.intensity = (1.5 * lightsOn) / numLights;
+                }
+            }
+        }
+
+        // You can now interact with the clickedObject, e.g., change its color
+        // clickedObject.material.color.set(0xff0000);
+    }
 }
 
 function start() {
@@ -120,10 +217,10 @@ function start() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const ambientlight = new THREE.AmbientLight(0xffffff, 1); // soft white light
+    ambientlight = new THREE.AmbientLight(0xffffff, 1.0); // soft white light
     scene.add(ambientlight);
 
-    const Hlight = new THREE.HemisphereLight(0xffffff, 0x000000, 3);
+    Hlight = new THREE.HemisphereLight(0xffffff, 0x000000, 1.5);
     scene.add(Hlight);
 
     // 5. Handle window resize
@@ -140,9 +237,11 @@ function start() {
     // Optional: Target the controls to a specific point (defaults to origin 0,0,0)
     controls.target.set(0, 1.5, 0);
 
-    scene.background = new THREE.Color(0.5, 0.7, 0.9);
+    scene.background = new THREE.Color(0, 0, 0);
 
     readSceneData();
+
+    document.addEventListener("dblclick", onDoubleClick, false);
 
     animate();
 }
