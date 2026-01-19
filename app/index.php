0336bfbd759322149045e19ab80c595a68b1de0c
Sergio <sergioteovit@gmail.com>
2026-01-19 12:31:15 -0600

    Cambio de Menu
    Se cambia código de menú para cargar desde archivos de configuración JSON

--- app/index.php
+++ app/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/">
     <title>Órganos y Sistemas</title>
     <!-- Incluye el CSS de Bootstrap 5 -->
     <!--link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"-->
@@ -13,7 +14,7 @@
 <div class="container">
     <div class="row">
         <?php
-        $servidor = "localhost";
+        /*$servidor = "localhost";
         $usuario = "myhvirtual";
         $contraseña = "uPLtaPntlDJnThpf";
         $nombre_base_de_datos = "myhvirtual";
@@ -49,7 +50,48 @@
         }
 
         // Cierra la conexión
-        $conn->close();
+        $conn->close();*/
+        
+        function readJsonFilesInDirectory($directoryPath) {
+            
+            $files = glob($directoryPath . '/*.json');
+            
+            if ($files === false) {
+                echo "<h3>No se encontraron archivos de configuración.</h3>";
+            }
+            
+            echo '<div class="carousel">';
+            
+            foreach ($files as $filePath) {
+                if (is_file($filePath)) {
+                    $jsonString = file_get_contents($filePath);
+                    
+                    if ($jsonString === false) {
+                        error_log("Error al leer el archivo");
+                    }
+                    
+                    $data = json_decode($jsonString, true);
+                    
+                    if ($data === null) {
+                        error_log("Error al decodificar el archivo JSON");
+                    }
+                    
+                    echo '<div class="carousel-item" style="background-image: url(icons/main/' . $data['background-image'] . ');">';
+                    
+                    echo "<h3>" . $data['name'] . '<br/><a class="waves-effect waves-light btn btn-large light-blue accent-2" href="region/php/levels.php?url='. $data['url'] .'" width="100%">Explorar</a></h3>';
+                    
+                    echo "</div>";
+                    
+                }
+            }
+            
+            echo "</div>";
+            
+        }
+        
+        $directory = 'json';
+        readJsonFilesInDirectory($directory);
+        
         ?>
     </div>
 </div>
